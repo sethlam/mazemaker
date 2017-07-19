fc1faa87e764f1dbbf9eac3c39de070a54d667d3
sethlam <sethlam1206@gmail.com>
2017-07-18 20:27:06 -0400

    first commit

---
+++ core/src/Scenes/HUD.java
@@ -0,0 +1,52 @@
+package Scenes;
+
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.g2d.BitmapFont;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.scenes.scene2d.Stage;
+import com.badlogic.gdx.scenes.scene2d.ui.Label;
+import com.badlogic.gdx.scenes.scene2d.ui.Table;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.sethlam.mazemaker.MazeMaker;
+
+import javax.swing.text.View;
+
+public class HUD {
+    public Stage stage;
+    private Viewport viewport;
+
+    private Integer worldTimer;
+    private float timeCount;
+    private Integer score;
+
+    Label countdownLabel;
+    Label scoreLabel;
+    Label timeLabel;
+    Label marioLabel;
+
+    public HUD (SpriteBatch sb) {
+        worldTimer = 300;
+        timeCount = 0;
+        score = 0;
+
+        viewport = new FitViewport(MazeMaker.V_WIDTH, MazeMaker.V_HEIGHT, new OrthographicCamera());
+        stage = new Stage(viewport, sb);
+
+        Table table = new Table();
+        table.top();
+        table.setFillParent(true);
+
+        countdownLabel = new Label(String.format("%03d", worldTimer), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        scoreLabel = new Label(String.format("%03d", score), new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+        timeLabel = new Label("TIME", new Label.LabelStyle(new BitmapFont(), Color.WHITE));
+
+        table.add(countdownLabel).expandX().padTop(10);
+        table.add(scoreLabel).expandX().padTop(10);
+        table.add(timeLabel).expandX().padTop(10);
+        table.row();
+
+        stage.addActor(table);
+    }
+}
---
+++ core/src/Screens/PlayScreen.java
@@ -0,0 +1,64 @@
+package Screens;
+
+import Scenes.HUD;
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.ScreenViewport;
+import com.badlogic.gdx.utils.viewport.StretchViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.sethlam.mazemaker.MazeMaker;
+
+public class PlayScreen implements Screen {
+    private MazeMaker game;
+    private HUD hud;
+    private OrthographicCamera gameCam;
+    private Viewport gamePort;
+    public PlayScreen(MazeMaker game) {
+        this.game = game;
+        gameCam = new OrthographicCamera();
+        gamePort = new FitViewport( MazeMaker.V_WIDTH, MazeMaker.V_HEIGHT, gameCam);
+        hud = new HUD(game.batch);
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+        Gdx.gl.glClearColor(0,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gamePort.update(width, height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
